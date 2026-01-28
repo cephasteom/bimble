b0dc33f5f4adedf85aba7b2c9fcedce17eb7e7ed
Peter Thomas <petethomasmusique@gmail.com>
2026-01-28 16:05:36 +0000

    reduce n of sequencer

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -1,7 +1,7 @@
 import { writable, derived } from "svelte/store";
 import { persist } from "./localstorage";
 
-export const sequencers = 4;
+export const sequencers = 3;
 export const bars = 2;
 export const timeSignature = writable<number>(4); // denominator of time signature
 timeSignature.subscribe(persist('bs.timeSignature'));
